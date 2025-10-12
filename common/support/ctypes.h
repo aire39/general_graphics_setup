293066a061fd600ef096058f7ee671198ea4aa5b
Sean Butler <seanxbutler@gmail.com>
2025-10-11 23:47:55 -0700

    wrong clang version checked against

--- common/support/ctypes.h
+++ common/support/ctypes.h
@@ -1,14 +1,14 @@
 #pragma once
 
 #if defined(__cpp_lib_parallel_algorithm) && __cpp_lib_parallel_algorithm >= 201703L || (defined(__cpp_lib_ranges) && __cpp_lib_ranges >= 201911L) \
-&& ((defined(__GNUC__) && __GNUC__ >= 12) || (defined(__clang__) && __clang_major__ >= 15) || (defined(_MSC_VER) && _MSC_VER >= 1930))
+&& ((defined(__GNUC__) && __GNUC__ >= 12) || (defined(__clang__) && __clang_major__ > 21) || (defined(_MSC_VER) && _MSC_VER >= 1930))
 #include <ranges>
 #else
 #include <range/v3/all.hpp>
 #endif
 
 #if defined(__cpp_lib_parallel_algorithm) && __cpp_lib_parallel_algorithm >= 201703L || (defined(__cpp_lib_ranges) && __cpp_lib_ranges >= 201911L) \
-&& ((defined(__GNUC__) && __GNUC__ >= 12) || (defined(__clang__) && __clang_major__ >= 15) || (defined(_MSC_VER) && _MSC_VER >= 1930))
+&& ((defined(__GNUC__) && __GNUC__ >= 12) || (defined(__clang__) && __clang_major__ > 21) || (defined(_MSC_VER) && _MSC_VER >= 1930))
 namespace gss {
   namespace views = std::views;
 }
