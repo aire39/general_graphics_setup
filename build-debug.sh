12f343742dc387d650952625edd0e79fb8d80eb3
Sean Butler <seanxbutler@gmail.com>
2025-10-19 11:08:42 -0700

    update build script
    change minimal cmake requirement for example to 3.22

--- build-debug.sh
+++ build-debug.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+set -e
+
+rm -rf build-debug
 mkdir build-debug
 cd build-debug
-cmake -DBUILD_ALL_EXAMPLES=ON -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build . --config=debug
+
+if command -v ninja > /dev/null 2>&1; then
+	echo "Using Ninja Generator"
+	CMAKE_GEN='-G Ninja'
+else
+	echo "Using Unix Makefiles Generator"
+	CMAKE_GEN='-G Unix Makefiles'
+fi
+
+cmake $CMAKE_GEN -DBUILD_ALL_EXAMPLES=ON -DCMAKE_BUILD_TYPE=Debug ..
+
+
+if command -v ninja > /dev/null 2>&1; then
+	cmake --build . --config=debug
+else
+	cmake --build . --config=debug -- -j
+fi
+
 cd ..
--- build-release.sh
+++ build-release.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+set -e
+
+rm -rf build-release
 mkdir build-release
 cd build-release
-cmake -DBUILD_ALL_EXAMPLES=ON -DCMAKE_BUILD_TYPE=Release ..
-cmake --build . --config=release
+
+if command -v ninja > /dev/null 2>&1; then
+	echo "Using Ninja Generator"
+	CMAKE_GEN='-G Ninja'
+else
+	echo "Using Unix Makefiles Generator"
+	CMAKE_GEN='-G Unix Makefiles'
+fi
+
+cmake $CMAKE_GEN -DBUILD_ALL_EXAMPLES=ON -DCMAKE_BUILD_TYPE=Release ..
+
+
+if command -v ninja > /dev/null 2>&1; then
+	cmake --build . --config=release
+else
+	cmake --build . --config=release -- -j
+fi
+
 cd ..
